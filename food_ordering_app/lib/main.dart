db16b082d0a125e3dfe8e7df1188f597844ae5ef
Benedict Osei Sefa <benedictoseisefa@Benedicts-MacBook-Pro.local>
2022-07-16 07:37:29 +0000

    created a colors.dart file and placed in background color and primary color

--- food_ordering_app/lib/main.dart
+++ food_ordering_app/lib/main.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:food_ordering_app/screens/home/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Food Delivery App',
       home: homePage(),
     );
